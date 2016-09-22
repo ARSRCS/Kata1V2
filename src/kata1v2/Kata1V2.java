e0219abefb034e7a765f072c9a9b820710982f9d
usuario <usuario@10.22.143.48>
2016-09-22 12:05:19 +0100

    Segunda version de Kata

--- src/kata1v2/Kata1V2.java
+++ src/kata1v2/Kata1V2.java
@@ -8,9 +8,9 @@ public class Kata1V2 {
     public static void main(String[] args) {
     Calendar date;
     date = GregorianCalendar.getInstance();
-    date.set(1992,8,17);
+    date.set(1992,8,16);
     
-    Person miPerson = new Person("Aresti",date);
+    Person miPerson = new Person("Aresti",date,"Hola");
         System.out.println(miPerson.getName() + " tiene " + miPerson.getAge() + " años");
     }
 }
